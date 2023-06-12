5ae4e9227c91f7627c14049e2a0a5dbefa63ac96
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-06-12 23:29:02 +0300

    feat: migrate sources.js to ts

---
+++ Migration-to-TypeScript/src/components/view/sources/sources.ts
@@ -0,0 +1,23 @@
+import './sources.css';
+import { Source } from '../../../types/types';
+import { getElement } from '../../../types/types';
+
+class Sources {
+    draw(data: Source[]) {
+        const fragment = document.createDocumentFragment();
+        const sourceItemTemp = getElement<HTMLElement>(document.body,'#sourceItemTemp') as HTMLTemplateElement;
+
+        data.forEach((item) => {
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
+
+            getElement<HTMLElement>(sourceClone, '.source__item-name').textContent = item.name;
+            getElement<HTMLElement>(sourceClone, '.source__item').setAttribute('data-source-id', item.id);
+
+            fragment.append(sourceClone);
+        });
+
+        getElement<HTMLElement>(document.body,'.sources').append(fragment);
+    }
+}
+
+export default Sources;
