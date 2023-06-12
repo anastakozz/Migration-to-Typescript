d68a0b5302b0ca1912c24b9b731a5470dfcbc269
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-06-12 15:23:45 +0300

    feat: add interfaces in type.ts

---
+++ Migration-to-TypeScript/src/types/types.ts
@@ -0,0 +1,31 @@
+export interface NewsResponse {
+    status: string,
+    totalResults: number,
+    articles: Article[]
+}
+
+export interface Article {
+    source: {id: string, name: string},
+    author: string,
+    title: string,
+    description: string,
+    url: string,
+    urlToImage: string,
+    publishedAt: string,
+    content: string
+}
+
+export interface SourcesResponse {
+    status: string,
+    sources: Source[]
+}
+
+export interface Source {
+    id: string,
+    name: string,
+    description: string,
+    url: string,
+    category: string,
+    language: string,
+    country: string
+}
