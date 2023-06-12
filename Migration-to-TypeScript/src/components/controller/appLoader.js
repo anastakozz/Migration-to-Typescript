c2ac57740a20e0b71a057ff5adf176edfdba9f85
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-06-12 15:31:07 +0300

    fix: change api for deploy

--- Migration-to-TypeScript/src/components/controller/appLoader.js
+++ Migration-to-TypeScript/src/components/controller/appLoader.js
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: '45f726bbf30f4c21aa1c4afb490448b6'
         });
     }
