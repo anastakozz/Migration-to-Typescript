8e3a438544703ff88a59ae15bac984950d8fd102
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-06-15 02:17:50 +0300

    fix: change ApiKey

--- Migration-to-TypeScript/src/components/controller/appLoader.ts
+++ Migration-to-TypeScript/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader'
 class AppLoader extends Loader {
   constructor () {
     super('https://rss-news-api.onrender.com/', {
-      apiKey: '45f726bbf30f4c21aa1c4afb490448b6'
+      apiKey: '69c66c08747f4db0affa02479db5a460'
     })
   }
 }
