fd8c7f46136cf085b8a063e0aedb64897bc3ef34
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-06-15 00:16:27 +0300

    fix: tune eslint config

--- Migration-to-TypeScript/.eslintrc.js
+++ Migration-to-TypeScript/.eslintrc.js
@@ -1,23 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "@typescript-eslint/no-explicit-any": 2
-    }
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['standard-with-typescript', "eslint:recommended", "plugin:@typescript-eslint/recommended"] ,
+  overrides: [
+  ],
+  parser: "@typescript-eslint/parser",
+  
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  rules: { 
+    "@typescript-eslint/no-explicit-any": 2
+  },
+
+  plugins: ["@typescript-eslint"]
 }
