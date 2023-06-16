05e12025a0bc7cab19e6b809c302a8967a8af8ed
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-06-16 11:23:52 +0300

    fix: remove type assertion

--- Migration-to-TypeScript/src/components/view/news/news.ts
+++ Migration-to-TypeScript/src/components/view/news/news.ts
@@ -1,5 +1,5 @@
 import './news.css'
-import { type ArticleI, getElement } from '../../../types/types'
+import { type ArticleI, getElement, isHtmlElement } from '../../../types/types'
 
 class News {
   draw (data: ArticleI[]): void {
@@ -10,25 +10,27 @@ class News {
 
     news.forEach((item, idx: number) => {
       if (newsItemTemp && newsItemTemp instanceof HTMLTemplateElement) {
-        const newsClone = newsItemTemp.content.cloneNode(true) as HTMLElement
-        if (idx % 2) getElement<HTMLElement>(newsClone, '.news__item').classList.add('alt')
+        const newsClone = newsItemTemp.content.cloneNode(true)
+        if (isHtmlElement(newsClone)) {
+          if (idx % 2) getElement<HTMLElement>(newsClone, '.news__item').classList.add('alt')
 
-        getElement<HTMLElement>(newsClone, '.news__meta-photo').style.backgroundImage = `url(${
+          getElement<HTMLElement>(newsClone, '.news__meta-photo').style.backgroundImage = `url(${
                 item.urlToImage || 'img/news_placeholder.jpg'
             })`
-        getElement<HTMLElement>(newsClone, '.news__meta-author').textContent = item.author || item.source.name
-        getElement<HTMLElement>(newsClone, '.news__meta-date').textContent = item.publishedAt
-          .slice(0, 10)
-          .split('-')
-          .reverse()
-          .join('-')
+          getElement<HTMLElement>(newsClone, '.news__meta-author').textContent = item.author || item.source.name
+          getElement<HTMLElement>(newsClone, '.news__meta-date').textContent = item.publishedAt
+            .slice(0, 10)
+            .split('-')
+            .reverse()
+            .join('-')
 
-        getElement<HTMLElement>(newsClone, '.news__description-title').textContent = item.title
-        getElement<HTMLElement>(newsClone, '.news__description-source').textContent = item.source.name
-        getElement<HTMLElement>(newsClone, '.news__description-content').textContent = item.description
-        getElement<HTMLElement>(newsClone, '.news__read-more a').setAttribute('href', item.url)
+          getElement<HTMLElement>(newsClone, '.news__description-title').textContent = item.title
+          getElement<HTMLElement>(newsClone, '.news__description-source').textContent = item.source.name
+          getElement<HTMLElement>(newsClone, '.news__description-content').textContent = item.description
+          getElement<HTMLElement>(newsClone, '.news__read-more a').setAttribute('href', item.url)
 
-        fragment.append(newsClone)
+          fragment.append(newsClone)
+        }
       }
     })
     const newsHtml = getElement<HTMLElement>(document.body, '.news')
